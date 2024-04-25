a213e873f55586f24b949a2c733f03e769d797c1
Frederick Castaneda Jr <frederickdev04@gmail.com>
2024-04-25 16:38:32 +0800

    fix: deployment warning

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console.log statements in production
+        pure_funcs: ["console.log"], // Treat console.log as pure function
+      },
+    },
+    chunkSizeWarningLimit: 500, // Adjust chunk size warning limit
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Example: Separate React and ReactDOM into separate chunk
           if (
             id.includes("node_modules/react/") ||
             id.includes("node_modules/react-dom/")
           ) {
             return "react-vendor";
           }
+          // Add additional manual chunking logic here as needed
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Adjust as needed
+    // Optionally configure additional optimizations
+    // optimizeDeps: {
+    //   include: ['package-name'], // Explicitly specify dependencies to optimize
+    // },
   },
 });
