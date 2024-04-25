547c4a5c5a43a687ccfeb1176cd5d44404eed7c1
Frederick Castaneda Jr <frederickdev04@gmail.com>
2024-04-25 16:33:33 +0800

    fix: chunk warning for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example: Separate React and ReactDOM into separate chunk
+          if (
+            id.includes("node_modules/react/") ||
+            id.includes("node_modules/react-dom/")
+          ) {
+            return "react-vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust as needed
+  },
+});
