64f6e0b927675c6d83576cc551ac4c050c8adbc5
Frederick Castaneda Jr <frederickdev04@gmail.com>
2024-04-25 16:47:54 +0800

    fix: deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        pure_funcs: ["console.log"],
+      },
+    },
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
-        manualChunks(id: string) {
-          // creating a chunk to @open-ish deps. Reducing the vendor chunk size
-          if (id.includes("@open-ish") || id.includes("tslib")) {
-            return "@open-ish";
-          }
-          // creating a chunk to react routes deps. Reducing the vendor chunk size
-          if (
-            id.includes("react-router-dom") ||
-            id.includes("@remix-run") ||
-            id.includes("react-router")
-          ) {
-            return "@react-router";
+        manualChunks(id) {
+          // Separate third-party libraries into a separate chunk
+          if (id.includes("node_modules/")) {
+            return "vendor";
           }
         },
       },
